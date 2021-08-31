683ba530e17a8aaa39261e7122710cb86de8a6ab
riki karasawa <fabulous.riki@gmail.com>
2021-08-31 09:15:20 +0900

    リファクタ

--- js/common.js
+++ js/common.js
@@ -1,9 +1,16 @@
 window.onload = () => {
+    // デバイス別でclass差替
     if (window.innerWidth <= 620) {
-        const appChild = document.querySelector('#app > .row').firstElementChild
-        appChild.classList.remove('col-12')
-        appChild.classList.remove('offset-6')
-        appChild.classList.add('col-20')
-        appChild.classList.add('offset-2')
+        document.getElementById('outer-wrap').classList.value = "col-20 offset-2 bg-white pt-5"
+        document.getElementById('inner-wrap').classList.value = "col-20 offset-2"
     }
+
+    // コピーイベント
+    document.querySelector('#copyText + button').addEventListener('click', () => {
+        const copyText = document.getElementById("copyText")
+        copyText.select()
+        document.execCommand("Copy")
+
+        alert("コピーしました！ : " + copyText.value)
+    })
 }
