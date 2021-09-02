e28dc8552ced3a08c5c54966f21734927a3ee65c
riki karasawa <fabulous.riki@gmail.com>
2021-09-02 23:53:00 +0900

    リファクタ

--- js/common.js
+++ js/common.js
@@ -2,6 +2,7 @@ window.onload = () => {
     // デバイス別でclass差替
     if (window.innerWidth <= 620) {
         document.getElementById('outer-wrap').classList.value = "col-20 offset-2 bg-white b-shadow pt-5"
+        document.getElementById('textarea').classList.value = "text-end m-auto"
         document.getElementById('inner-wrap').classList.value = "col-20 offset-2"
     }
 
