53ea955e54aa1400ad90550e8a045914fe934535
riki karasawa <fabulous.riki@gmail.com>
2021-12-26 11:55:23 +0900

    Localstrege：ローディングイベント

--- js/common.js
+++ js/common.js
@@ -4,23 +4,23 @@
 $(function() {
     var h = $(window).height();
 
-    $('#wrap').css('display','none');
+    $('#app').css('display','none');
     $('#loading ,#spinner').height(h).css('display','block');
 });
 
 $(window).on('load',function () {
-    $('#wrap').css('display', 'block');
+    $('#app').css('display', 'block');
     $('#loading').delay(500).fadeOut(500);
     $('#spinner').delay(300).fadeOut(300);
 });
 
 //5秒たったら強制的にロード画面を非表示
 $(function() {
-    setTimeout('stopload()',800);
+    setTimeout('stopload()',600);
 });
 
 function stopload() {
-    $('#wrap').css('display','block');
+    $('#app').css('display','block');
     $('#loading').delay(500).fadeOut(500);
     $('#spinner').delay(300).fadeOut(300);
 }
